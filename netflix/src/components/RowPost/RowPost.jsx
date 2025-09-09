8cac7cb6d91c86d11fd8cbd9b1eb05f385749050
Sharafath_V <142240872+sharafath07@users.noreply.github.com>
2025-09-09 14:30:31 +0000

    API integration

--- netflix/src/components/RowPost/RowPost.jsx
+++ netflix/src/components/RowPost/RowPost.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import './RowPost.css';
-import { imageUrl } from '../../constants/constants';
+import Youtube from 'react-youtube';
+import { API_KEY, imageUrl } from '../../constants/constants';
 import axios from '../../axios';                
 
 function RowPost({title,  isSmall, url}) {
 
   const [movies, setmovies] = useState([]);
+  const [urlId, setUrlId] = useState('');
 
   useEffect(()=>{
     axios.get(url).then((response)=>{
@@ -14,6 +16,30 @@ function RowPost({title,  isSmall, url}) {
     })
   },[])
 
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      autoplay: 1
+    }
+  }
+
+  function handleMovie(id){
+    axios.get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then((response)=>{
+      console.log(response.data.results[0].key);
+      if (response.data.results.length !== 0) {
+        setUrlId(response.data.results[0])
+      }else{
+        console.log("No video in the Backend");
+      }
+      
+      return (
+        <Youtube videoId={response.data.results[0].key} />
+      )
+    })
+    
+
+  }
   return (
     <div className='row'>
       <h2 className="title">{title}</h2>
@@ -21,10 +47,11 @@ function RowPost({title,  isSmall, url}) {
         {movies.map((movie)=>{
 
           return (
-            <img className={isSmall ? 'poster smallPoster': 'poster'} src={movie ? imageUrl + movie.poster_path: ''} alt="poster" />
+            <img onClick={()=>{handleMovie(movie.id)}} className={isSmall ? 'poster smallPoster': 'poster'} src={movie ? imageUrl + movie.poster_path: ''} alt="poster" />
           )
         })}         
       </div>
+      { urlId && <Youtube videoId={urlId.key} opts={opts} />}
     </div>
   )
 }
